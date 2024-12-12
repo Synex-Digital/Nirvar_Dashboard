dff80977b0490e08706432cb564ddbaacbd8610d
Esmail Khalifa <hossainmdismail9010@gmail.com>
2024-12-12 22:28:48 +0600

    hgt,wgt require null -  khaalifa

--- app/Http/Controllers/Api/PatientProfileController.php
+++ app/Http/Controllers/Api/PatientProfileController.php
@@ -13,14 +13,15 @@ use Illuminate\Support\Facades\Validator;
 
 class PatientProfileController extends Controller
 {
-    public function profile($id){
-        $user = User::where('id',$id)->where('role','patient')->first();
-        if(is_null($user)){
+    public function profile($id)
+    {
+        $user = User::where('id', $id)->where('role', 'patient')->first();
+        if (is_null($user)) {
             return response()->json([
                 'status'    => 0,
                 'message'   => "User not found",
-            ],200);
-        }else{
+            ], 200);
+        } else {
             $data = $user->patient->weight_height;
 
             // Initialize variables
@@ -28,7 +29,7 @@ class PatientProfileController extends Controller
             $height = null;
 
             // Check for weight and height pattern
-            if($data){
+            if ($data) {
                 if (preg_match('/^(\d+),?\s*(\d+)\s*FT\.?\s*(\d*)\s*IN?$/i', $data, $matches)) {
                     $weight = !empty($matches[1]) ? $matches[1] : null;
                     $height = trim($matches[2] . ' FT ' . $matches[3] . ' IN');
@@ -57,7 +58,7 @@ class PatientProfileController extends Controller
                     'name'          => $user->name,
                     'email'         => $user->email,
                     'number'        => $user->number,
-                    'photo'         =>url('uploads/patient/profile/'.$user->photo) ,
+                    'photo'         => url('uploads/patient/profile/' . $user->photo),
                     'blood_gorup'   => $user->patient->blood_group,
                     'date_of_birth' => $user->patient->date_of_birth,
                     'age'           => $user->patient->date_of_birth ? \Carbon\Carbon::parse($user->patient->date_of_birth)->age : null,
@@ -68,13 +69,14 @@ class PatientProfileController extends Controller
                 ]
 
 
-            ],200);
+            ], 200);
             // return response()->json($user);
         }
     }
 
-    public function profile_register(Request $request){
-        $validate = Validator::make($request->all(),[
+    public function profile_register(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
             'user_id'       => 'required',
             'name'          => 'required',
             'email'         => $request->email ? 'required|email|unique:users,email' : 'nullable',
@@ -83,7 +85,7 @@ class PatientProfileController extends Controller
             'date_of_birth' => 'required',
             'blood_group'   => 'required',
             'photo'         => $request->photo ? 'mimes:jpg,jpeg,png,webp,heif' : 'nullable',
-        ],[
+        ], [
             'user_id.required'          => 'User id is required',
             'name.required'             => 'Name is required',
             'email.required'            => 'Email is required',
@@ -98,22 +100,22 @@ class PatientProfileController extends Controller
             return response()->json([
                 'status'    => 0,
                 'message'   => $validate->errors()->messages(),
-            ],200);
+            ], 200);
         }
-        $user = User::where('id',$request->user_id)->where('role','patient')->first();
-        if(is_null($user)){
+        $user = User::where('id', $request->user_id)->where('role', 'patient')->first();
+        if (is_null($user)) {
             return response()->json([
                 'status'    => 0,
                 'message'   => "User not found",
-            ],200);
-        }else{
+            ], 200);
+        } else {
 
             $user->name     = $request->name;
-            $user->email    = $request->email?? null;
+            $user->email    = $request->email ?? null;
             $user->password = Hash::make($request->password);
             $uploaded_file  = $request->file('photo');
             $extn           = $uploaded_file?->getClientOriginalExtension();
-            $fileName       = 'PROFILE_'.rand(100000,999999) . '.' . $extn;
+            $fileName       = 'PROFILE_' . rand(100000, 999999) . '.' . $extn;
             $uploaded_file?->move(public_path('uploads/patient/profile/'), $fileName);
             $user->photo    = $uploaded_file ?  $fileName : null;
             $user->save();
@@ -151,60 +153,54 @@ class PatientProfileController extends Controller
                 'status'    => 1,
                 'message'   => "Profile updated successfully",
                 'number'   => $user->number,
-            ],200);
+            ], 200);
         }
-
     }
-    public function profile_update(Request $request){
+    public function profile_update(Request $request)
+    {
         $user = User::find(auth()->user()->id);
-        $validate = Validator::make($request->all(),[
+        $validate = Validator::make($request->all(), [
             'photo'         => 'required|mimes:jpg,jpeg,png,webp,heif',
             'name'          => 'required',
-            'email'         => 'required|email|unique:users,email,'.$user->id,
+            'email'         => 'required|email|unique:users,email,' . $user->id,
             'gender'        => 'required',
             'date_of_birth' => 'required',
             'blood_group'   => 'required',
-            'weight'        => 'required',
-            'height_ft'     => 'required',
-            'height_in'     => 'required',
             'address'       => 'required',
 
-        ],[
+        ], [
             'name.required'             => 'Name is required',
             'email.required'            => 'Email is required',
             'email.email'               => 'Email must be a valid email address',
             'gender.required'           => 'Gender is required',
             'date_of_birth.required'    => 'Date of birth is required',
             'blood_group.required'      => 'Blood group is required',
-            'weight.required'           => 'Weight is required',
-            'height_ft.required'        => 'Height ft is required',
-            'height_in.required'        => 'Height in is required',
             'address.required'          => 'Address is required',
         ]);
         if ($validate->fails()) {
             return response()->json([
                 'status'    => 0,
                 'message'   => $validate->errors()->messages(),
-            ],200);
+            ], 200);
         }
 
-        if(is_null($user)){
+        if (is_null($user)) {
             return response()->json([
                 'status'    => 0,
                 'message'   => "User not found",
-            ],200);
-        }else{
+            ], 200);
+        } else {
             $user->name     = $request->name;
             $user->email    = $request->email;
-            if($user->photo !== null){
-               $path = public_path('uploads/patient/profile/'.$user->photo);
-               unlink($path);
+            if ($user->photo !== null) {
+                $path = public_path('uploads/patient/profile/' . $user->photo);
+                unlink($path);
             }
             $uploaded_file  = $request->file('photo');
             $extn           = $uploaded_file->getClientOriginalExtension();
-            $fileName       = 'PROFILE_'.rand(100000,999999) . '.' . $extn;
+            $fileName       = 'PROFILE_' . rand(100000, 999999) . '.' . $extn;
             $uploaded_file->move(public_path('uploads/patient/profile/'), $fileName);
-            $user->photo    =$fileName;
+            $user->photo    = $fileName;
             $user->save();
             $patient = $user->patient;
             $patient->blood_group   = $request->blood_group;
@@ -239,22 +235,22 @@ class PatientProfileController extends Controller
             return response()->json([
                 'status'    => 1,
                 'message'   => "Profile updated successfully",
-            ],200);
+            ], 200);
         }
-
     }
-    public function search(Request $request){
-        $validate = Validator::make($request->all(),[
+    public function search(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
             'search_data' => 'required',
 
-        ],[
+        ], [
             'search_data.required' => 'Search input cannot be empty',
         ]);
         if ($validate->fails()) {
             return response()->json([
                 'status'    => 0,
                 'message'   => $validate->errors()->messages(),
-            ],200);
+            ], 200);
         }
         $user_id = auth('api')->user()->id;
         $search_data = $request->search_data;
@@ -266,48 +262,49 @@ class PatientProfileController extends Controller
 
         // Fetch files that belong to the user's folders and match the search criteria
         $files = File::whereHas('folder', function ($query) use ($user_id) {
-                $query->where('user_id', $user_id);
-            })
+            $query->where('user_id', $user_id);
+        })
             ->where('name', 'like', '%' . $search_data . '%')
             ->get();
 
-            // Prepare the response
-            return response()->json([
-                'status' => 1,
-                'message' => "success",
-                'data'    => [
-                    'folders' => $folders->map(function ($folder) {
-                        return [
-                            'id'            => $folder->id,
-                            'user_id'       => $folder->user_id,
-                            'name'          => $folder->name,
-                            'file_count'    => count($folder->files),
-                            'created_at'    => $folder->created_at->format('d-M-y'),
-                        ];
-                    }),
-                    'files' => $files->map(function ($file) {
-                        $rename = $this->splitFileName($file->name);
-                        $renameName = $rename ? $rename['name'] : null;
-                        return [
-                            'folder_id'     => $file->folder_id,
-                            'folder_name'   => $file->folder->name,
-                            'id'            => $file->id,
-                            'name'          => $file->name,
-                            'rename'        => $renameName,
-                            'type'          => $file->type,
-                            'path'          => url('uploads/patient/files/' . $file->name),
-                            'created_at'    => $file->created_at->format('d-M-y'),
-                        ];
-                    }),
-                ]
+        // Prepare the response
+        return response()->json([
+            'status' => 1,
+            'message' => "success",
+            'data'    => [
+                'folders' => $folders->map(function ($folder) {
+                    return [
+                        'id'            => $folder->id,
+                        'user_id'       => $folder->user_id,
+                        'name'          => $folder->name,
+                        'file_count'    => count($folder->files),
+                        'created_at'    => $folder->created_at->format('d-M-y'),
+                    ];
+                }),
+                'files' => $files->map(function ($file) {
+                    $rename = $this->splitFileName($file->name);
+                    $renameName = $rename ? $rename['name'] : null;
+                    return [
+                        'folder_id'     => $file->folder_id,
+                        'folder_name'   => $file->folder->name,
+                        'id'            => $file->id,
+                        'name'          => $file->name,
+                        'rename'        => $renameName,
+                        'type'          => $file->type,
+                        'path'          => url('uploads/patient/files/' . $file->name),
+                        'created_at'    => $file->created_at->format('d-M-y'),
+                    ];
+                }),
+            ]
 
-            ], 200);
+        ], 200);
     }
-    public function password_change(Request $request){
+    public function password_change(Request $request)
+    {
         $validate = Validator::make($request->all(), [
             'old_password' => 'required',
             'new_password' => 'required|min:6',
-        ],[
+        ], [
             'old_password' => 'Old password is required',
             'new_password'  => 'New password is required',
 
@@ -316,7 +313,7 @@ class PatientProfileController extends Controller
             return response()->json([
                 'status'    => 0,
                 'message'   => $validate->errors()->messages(),
-            ],200);
+            ], 200);
         }
 
         $user = User::find(auth('api')->user()->id);
@@ -324,20 +321,18 @@ class PatientProfileController extends Controller
             return response()->json([
                 'status'    => 0,
                 'message'   => 'Old password is incorrect',
-            ],200);
-        }else{
+            ], 200);
+        } else {
             $user->password = Hash::make($request->new_password);
             $user->save();
             return response()->json([
                 'status'    => 1,
                 'message'   => 'Password changed successfully',
-            ],200);
+            ], 200);
         }
-
-
-
     }
-    function splitFileName($filename) {
+    function splitFileName($filename)
+    {
         $pattern = '/(.+)(_PR-\d{4}|_TR-\d{4})(\.\w+)$/i'; // Regex to extract the base name and the code
         preg_match($pattern, $filename, $matches);
 
@@ -351,5 +346,4 @@ class PatientProfileController extends Controller
 
         return null; // Return null if the pattern does not match
     }
-
 }
